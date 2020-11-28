dc1a0149ab644a7b5b783ddece6bcdbcca8b59b2
ednailsonc <ednailsoncunha@gmail.com>
2020-11-28 17:53:25 -0300

    # Discount calculator finalizing

--- discount-calculator/app/config.go
+++ discount-calculator/app/config.go
@@ -1,6 +1,10 @@
 package app
 
-import "github.com/ednailson/hash-challenge/discount-calculator/database"
+import (
+	"encoding/json"
+	"github.com/ednailson/hash-challenge/discount-calculator/database"
+	"os"
+)
 
 type Config struct {
 	Port     int      `json:"port"`
@@ -11,4 +15,45 @@ type Database struct {
 	Config            database.Config `json:"config"`
 	UserCollection    string          `json:"user_collection"`
 	ProductCollection string          `json:"product_collection"`
+}
+
+func NewConfigFile(filename string) error {
+	err := generateConfigFile(filename, configSample())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func generateConfigFile(filename string, config Config) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	data, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func configSample() Config {
+	return Config{
+		Port: 9000,
+		Database: Database{
+			Config: database.Config{
+				Host:     "http://arangodb.service.com.br",
+				Port:     8529,
+				User:     "root",
+				Password: "dummyPass",
+				Database: "hash-db",
+			},
+			UserCollection:    "user-collection",
+			ProductCollection: "product-collection",
+		},
+	}
 }
--- discount-calculator/go.mod
+++ discount-calculator/go.mod
@@ -5,8 +5,11 @@ go 1.15
 require (
 	github.com/arangodb/go-driver v0.0.0-20200624173407-d1c92a8bd2b8
 	github.com/golang/protobuf v1.4.3
+	github.com/micro/go-micro v1.18.0
 	github.com/onsi/gomega v1.10.3
 	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.7.0
+	github.com/urfave/cli v1.22.5
 	golang.org/x/net v0.0.0-20201006153459-a7d1128ccaa0
 	google.golang.org/grpc v1.33.2
 	google.golang.org/protobuf v1.25.0
